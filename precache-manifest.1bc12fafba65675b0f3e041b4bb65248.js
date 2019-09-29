89bb0edd7b1301ebb981d154e8d0b09eebbae8a9
Carlos Garcia <cgarciasantander@gmail.com>
2019-09-29 11:39:42 -0500

    Updates

---
+++ precache-manifest.1bc12fafba65675b0f3e041b4bb65248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e3d348c2be7dc35226c37918b697f7",
+    "url": "/alfred-react/index.html"
+  },
+  {
+    "revision": "6b0bfdfae5edc614f403",
+    "url": "/alfred-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "10ccab246b51cd9bf821",
+    "url": "/alfred-react/static/js/2.5b7809cd.chunk.js"
+  },
+  {
+    "revision": "6b0bfdfae5edc614f403",
+    "url": "/alfred-react/static/js/main.2f0fe33e.chunk.js"
+  },
+  {
+    "revision": "4539457b6b3a273a8085",
+    "url": "/alfred-react/static/js/runtime-main.a9040a7a.js"
+  },
+  {
+    "revision": "ce8b650f687c3e7e61d409f5371a0fb8",
+    "url": "/alfred-react/static/media/logo-horizontal.ce8b650f.png"
+  }
+]);
