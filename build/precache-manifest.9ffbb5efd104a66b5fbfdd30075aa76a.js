d3d4db78e3e5c21ae60ac2a55e88860b9045221a
Carlos Garcia <cgarciasantander@gmail.com>
2019-12-17 18:42:03 -0500

    Production build

---
+++ build/precache-manifest.9ffbb5efd104a66b5fbfdd30075aa76a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fe216cc87320cdb4fa52197fca74df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847a119ee57acdc78ac2",
+    "url": "/static/css/main.bf3615c7.chunk.css"
+  },
+  {
+    "revision": "c371bbb142329349acba",
+    "url": "/static/js/2.4fc0c658.chunk.js"
+  },
+  {
+    "revision": "847a119ee57acdc78ac2",
+    "url": "/static/js/main.34000452.chunk.js"
+  },
+  {
+    "revision": "6f20c68b12356cb61e6f",
+    "url": "/static/js/runtime-main.6fd48f80.js"
+  },
+  {
+    "revision": "90e65dff1bccb5f9312048424ce8f840",
+    "url": "/static/media/event1.90e65dff.jpg"
+  },
+  {
+    "revision": "dcd4c25510db586df8d9bc452c86d4e7",
+    "url": "/static/media/event2.dcd4c255.jpg"
+  },
+  {
+    "revision": "d6d8c86f09856ae1fb858be627871bdf",
+    "url": "/static/media/event3.d6d8c86f.png"
+  },
+  {
+    "revision": "ac6f15af3b3a3dee8c24ffbfa61baee8",
+    "url": "/static/media/event4.ac6f15af.png"
+  },
+  {
+    "revision": "f3a0e8da5033e6168f184ad66dde6205",
+    "url": "/static/media/food1.f3a0e8da.jpeg"
+  },
+  {
+    "revision": "256c7b659cd40069e3404e6023b94c8e",
+    "url": "/static/media/food2.256c7b65.jpeg"
+  },
+  {
+    "revision": "7325d197fa747ce885ebbd1bdcd11d61",
+    "url": "/static/media/logo-horizontal.7325d197.png"
+  },
+  {
+    "revision": "aa0b8d7022bd1c5afac521d16d0f1715",
+    "url": "/static/media/promo1.aa0b8d70.png"
+  },
+  {
+    "revision": "0bc6d93254640b0baa3ba9454c8815dd",
+    "url": "/static/media/promo2.0bc6d932.jpg"
+  },
+  {
+    "revision": "8769c954162ae514f7d6df916003ef6f",
+    "url": "/static/media/promo3.8769c954.jpeg"
+  },
+  {
+    "revision": "1eab25dde5ece8978a97a270ff49a7cd",
+    "url": "/static/media/promo4.1eab25dd.jpg"
+  }
+]);
